75484bc05524bcceba1827e5da1819342e215882
AttenotPierre <pierre.attenot7@etu.univ-lorraine.fr>
2025-09-18 10:40:54 +0200

    ajout Identite

---
+++ Identite.java
@@ -0,0 +1,28 @@
+public class Identite {
+    private String nip;
+    private String nom;
+    private String prenom;
+
+    public Identite(String nip, String nom, String prenom) {
+        this.nip = nip;
+        this.nom = nom;
+        this.prenom = prenom;
+    }
+
+    public String getNip() {
+        return nip;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    @Override
+    public String toString() {
+        return nip + " - " + nom + " " + prenom;
+    }
+}
